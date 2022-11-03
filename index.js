220f37fbde44bb37e9ea3fcb6ea178f9f90db79e
Laisses <71999249+Laisses@users.noreply.github.com>
2022-11-03 10:29:44 -0300

    index.js: create route /sign-up

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { USERS, TWEETS } from "./mock-data.js";
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(5000, () => {
     console.log("server running");
@@ -19,4 +21,10 @@ app.get("/tweets/:user", (req, res) => {
     const { user } = req.params;
     const tweetsUser = TWEETS.filter(t => user === t.username);
     res.status(200).send(tweetsUser);
-})
+});
+
+app.post("/sign-up", (req, res) => {
+    const {username, imageURL} = req.body;
+    USERS[username] = imageURL;
+    res.sendStatus(201);
+});
